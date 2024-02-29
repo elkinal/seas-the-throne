8b2a698458badfaf507c4ce55133f42872b3c0f8
tl558 <tl558@cornell.edu>
2024-02-29 11:32:10 -0500

    Refactored Renderable to not consider physics

--- core/src/main/java/edu/cornell/jade/seasthethrone/Renderable.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/Renderable.java
@@ -1,22 +1,8 @@
 package edu.cornell.jade.seasthethrone;
 
-import com.badlogic.gdx.physics.box2d.World;
-
-public class Renderable extends Obstacle{
-    RenderingEngine renderingEngine = new RenderingEngine();
-    @Override
-    public void draw(GameCanvas canvas) {
-        renderingEngine.addRenderable(this);
-    }
-
-    public float getX(){
-        return 0;
-    }
-    public float getY(){
-        return 0;
-    }
-    public void printPosition(){
-
-    }
-
+public interface Renderable {
+    void drawRenderable();
+    float getX();
+    float getY();
+    void printPosition();
 }
