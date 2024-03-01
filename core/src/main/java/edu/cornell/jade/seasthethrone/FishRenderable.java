43e132c30ac31ed7611e9f8771b99d8063eff37d
tl558 <tl558@cornell.edu>
2024-03-01 15:43:01 -0500

    Renderable Type Examples

---
+++ core/src/main/java/edu/cornell/jade/seasthethrone/FishRenderable.java
@@ -0,0 +1,16 @@
+package edu.cornell.jade.seasthethrone;
+
+public class FishRenderable implements Renderable{
+    @Override
+    public float getX() {
+        return 0;
+    }
+
+    @Override
+    public float getY() {
+        return 0;
+    }
+    public String getType(){
+        return "fish";
+    }
+}
---
+++ core/src/main/java/edu/cornell/jade/seasthethrone/PlayerRenderable.java
@@ -0,0 +1,18 @@
+package edu.cornell.jade.seasthethrone;
+
+public class PlayerRenderable implements Renderable{
+    @Override
+    public float getX() {
+        return 0;
+    }
+
+    @Override
+    public float getY() {
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return "player";
+    }
+}
