1b7901ff0bcf0b4c5c8902db1db985f79634a8de
tl558 <tl558@cornell.edu>
2024-03-02 14:26:23 -0500

    Rendering linked to GameCanvas

--- core/src/main/java/edu/cornell/jade/seasthethrone/render/RenderingEngine.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/render/RenderingEngine.java
@@ -1,5 +1,6 @@
 package edu.cornell.jade.seasthethrone.render;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.*;
 
 public class RenderingEngine {
@@ -13,7 +14,12 @@ public class RenderingEngine {
     renderables.add(r);
   }
 
-  public void drawRenderables() {
-
+  public void drawRenderables(GameCanvas canvas) {
+      //TODO
+      for (Renderable r: renderables){
+          Texture texture = null;
+          canvas.draw(texture, r.getX(), r.getY());
+                    throw new RuntimeException("unimplemented");
+      }
   }
 }
