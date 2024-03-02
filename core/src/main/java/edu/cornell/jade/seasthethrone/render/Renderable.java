8366e842cd478190d3784faaaa488c9ef487fde8
tl558 <tl558@cornell.edu>
2024-03-02 14:59:06 -0500

    Added Documentations

--- core/src/main/java/edu/cornell/jade/seasthethrone/render/Renderable.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/render/Renderable.java
@@ -2,7 +2,13 @@ package edu.cornell.jade.seasthethrone.render;
 
 public interface Renderable {
 
+  /**
+   * @return the X coordinate of the Renderable Object
+   */
   float getX();
 
+  /**
+   * @return the Y coordinate of the Renderable Object
+   */
   float getY();
 }
--- core/src/main/java/edu/cornell/jade/seasthethrone/render/RenderingEngine.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/render/RenderingEngine.java
@@ -4,22 +4,32 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.*;
 
 public class RenderingEngine {
-  Array<Renderable> renderables;
+    /**The Renderable objects for rendering*/
+    Array<Renderable> renderables;
 
-  public RenderingEngine() {
-    renderables = new Array<>();
-  }
-
-  public void addRenderable(Renderable r) {
-    renderables.add(r);
-  }
-
-  public void drawRenderables(GameCanvas canvas) {
-      //TODO
-      for (Renderable r: renderables){
-          Texture texture = null;
-          canvas.draw(texture, r.getX(), r.getY());
-                    throw new RuntimeException("unimplemented");
-      }
-  }
+    /**
+     * Creates a new RenderingEngine
+     */
+    public RenderingEngine() {
+        renderables = new Array<>();
+    }
+    /**
+     * Adds a renderable object to the list to be rendered
+     * @param r The renderable to be added
+     */
+    public void addRenderable(Renderable r) {
+        renderables.add(r);
+    }
+    /**
+     * Draws all the renderable objects in the list to be rendered
+     * @param canvas  Drawing Context
+     */
+    public void drawRenderables(GameCanvas canvas) {
+        //TODO
+        for (Renderable r : renderables) {
+            Texture texture = null;
+            canvas.draw(texture, r.getX(), r.getY());
+            throw new RuntimeException("unimplemented");
+        }
+    }
 }
