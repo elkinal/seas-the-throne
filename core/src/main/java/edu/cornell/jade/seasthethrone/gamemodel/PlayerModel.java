7b3d7a5521947a6b094d663fdfb99f6aab0ad7f0
jeremy ku-benjet <jk2582@cornell.edu>
2024-03-04 01:30:34 -0500

    autoformat

--- core/src/main/java/edu/cornell/jade/seasthethrone/gamemodel/PlayerModel.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/gamemodel/PlayerModel.java
@@ -14,135 +14,155 @@ import edu.cornell.jade.seasthethrone.updateengine.UpdateEngine;
  */
 public class PlayerModel extends ComplexModel {
 
-    /** Whether the player is dashing */
-    private boolean isDashing;
-    /** Frame counter for dashing. Tracks how long the player has been dashing for and how long until
-     * they can dash again. */
-    private int dashCounter;
-    /** The time limit (in frames) between dashes */
-    private int dashCooldownLimit;
+  /** Whether the player is dashing */
+  private boolean isDashing;
+  /**
+   * Frame counter for dashing. Tracks how long the player has been dashing for
+   * and how long until
+   * they can dash again.
+   */
+  private int dashCounter;
+  /** The time limit (in frames) between dashes */
+  private int dashCooldownLimit;
 
-    /** The number of frames a dash lasts */
-    private int dashLength;
+  /** The number of frames a dash lasts */
+  private int dashLength;
 
-    /** Unique identifier for the point sensor; used in collision handling */
-    private String pointSensorName;
+  /** Unique identifier for the point sensor; used in collision handling */
+  private String pointSensorName;
 
-    /** Scaling factor for player movement */
-    private float moveSpeed;
+  /** Scaling factor for player movement */
+  private float moveSpeed;
 
-    /**
-     * {@link PlayerModel} constructor using an x and y coordinate.
-     *
-     * @param x     The x-position for this player in world coordinates
-     * @param y     The y-position for this player in world coordinates
-     */
-    public PlayerModel(float x, float y){
-        super(x, y);
+  /**
+   * {@link PlayerModel} constructor using an x and y coordinate.
+   *
+   * @param x The x-position for this player in world coordinates
+   * @param y The y-position for this player in world coordinates
+   */
+  public PlayerModel(float x, float y) {
+    super(x, y);
 
-        //make a triangle for now
-        float vertices[] = new float[6];
-        vertices[0] = -0.5f;
-        vertices[1] = -1;
-        vertices[2] = 0.5f;
-        vertices[3] = -1;
-        vertices[4] = 0;
-        vertices[5] = 1;
+    // make a triangle for now
+    float vertices[] = new float[6];
+    vertices[0] = -0.5f;
+    vertices[1] = -1;
+    vertices[2] = 0.5f;
+    vertices[3] = -1;
+    vertices[4] = 0;
+    vertices[5] = 1;
 
-        // Set constants
-        moveSpeed = 20f;
-        dashCounter = 0;
-        dashCooldownLimit = 30;
-        dashLength = 30;
-        isDashing = false;
+    // Set constants
+    moveSpeed = 20f;
+    dashCounter = 0;
+    dashCooldownLimit = 30;
+    dashLength = 30;
+    isDashing = false;
 
-        PolygonModel nose = new PolygonModel(vertices);
-        nose.setName("nose");
-        nose.setBodyType(BodyDef.BodyType.DynamicBody);
-        bodies.add(nose);
-    }
+    PolygonModel nose = new PolygonModel(vertices);
+    nose.setName("nose");
+    nose.setBodyType(BodyDef.BodyType.DynamicBody);
+    bodies.add(nose);
+  }
 
-    /**
-     * Returns player's move speed.
-     *
-     *
-     * @return player's move speed.
-     * */
-    public float getMoveSpeed() {return moveSpeed;}
-    /**
-     * Sets player's move speed.
-     *
-     * @param speed new value for move speed.
-     * */
-    public void setMoveSpeed(float speed) {moveSpeed = speed;}
+  /**
+   * Returns player's move speed.
+   *
+   *
+   * @return player's move speed.
+   */
+  public float getMoveSpeed() {
+    return moveSpeed;
+  }
 
-    /**
-     * Returns true if the player is dashing.
-     *
-     * @return true if the player is dashing.
-     */
-    public boolean isDashing() {
-        return isDashing;
-    }
+  /**
+   * Sets player's move speed.
+   *
+   * @param speed new value for move speed.
+   */
+  public void setMoveSpeed(float speed) {
+    moveSpeed = speed;
+  }
 
-    /**
-     * Sets if the player is dashing
-     *
-     * @param value is the player dashing
-     * */
-    public void setDashing(boolean value) { isDashing = value; }
+  /**
+   * Returns true if the player is dashing.
+   *
+   * @return true if the player is dashing.
+   */
+  public boolean isDashing() {
+    return isDashing;
+  }
 
-    /** Returns if the player can dash */
-    public boolean canDash() {
-        return !isDashing && dashCounter == 0;
-    }
+  /**
+   * Sets if the player is dashing
+   *
+   * @param value is the player dashing
+   */
+  public void setDashing(boolean value) {
+    isDashing = value;
+  }
 
-    /** Sets value for dash cooldown */
-    public void setDashCounter(int value) { dashCounter = value; }
+  /** Returns if the player can dash */
+  public boolean canDash() {
+    return !isDashing && dashCounter == 0;
+  }
 
-    /** Returns current value of dash cooldown */
-    public int getDashCounter() { return dashCounter; }
+  /** Sets value for dash cooldown */
+  public void setDashCounter(int value) {
+    dashCounter = value;
+  }
 
-    /** Returns dash limit */
-    public int getDashCooldownLimit() { return dashCooldownLimit; }
+  /** Returns current value of dash cooldown */
+  public int getDashCounter() {
+    return dashCounter;
+  }
 
-    /** Decriments dash cooldown */
-    public void decrementDashCounter() { dashCounter -= 1; }
+  /** Returns dash limit */
+  public int getDashCooldownLimit() {
+    return dashCooldownLimit;
+  }
 
-    /** Returns length of dash in frames */
-    public int getDashLength() { return dashLength; }
+  /** Decriments dash cooldown */
+  public void decrementDashCounter() {
+    dashCounter -= 1;
+  }
 
+  /** Returns length of dash in frames */
+  public int getDashLength() {
+    return dashLength;
+  }
 
-    /**
-     * Returns the name of the nose point sensor
-     *
-     * This is used by ContactListener
-     *
-     * @return the name of the nose point sensor
-     */
-    public String getPointSensorName() {
-        return pointSensorName;
-    }
+  /**
+   * Returns the name of the nose point sensor
+   *
+   * This is used by ContactListener
+   *
+   * @return the name of the nose point sensor
+   */
+  public String getPointSensorName() {
+    return pointSensorName;
+  }
 
-    // built from multiple polygonmodels?
-    @Override
-    protected boolean createJoints(World world) {
+  // built from multiple polygonmodels?
+  @Override
+  protected boolean createJoints(World world) {
 
-        pointSensorName = "NosePointSensor";
+    pointSensorName = "NosePointSensor";
 
-        // Create sensor on the points of the "nose," this should be factored to a diff function later
-        Vector2 sensorCenter = new Vector2(0, 1);
-        FixtureDef sensorDef = new FixtureDef();
-        sensorDef.isSensor = true;
-        CircleShape sensorShape = new CircleShape();
-        //sensor has 0 radius, maybe modify?
-        sensorShape.setRadius(0);
-        sensorShape.setPosition(sensorCenter);
-        sensorDef.shape = sensorShape;
+    // Create sensor on the points of the "nose," this should be factored to a diff
+    // function later
+    Vector2 sensorCenter = new Vector2(0, 1);
+    FixtureDef sensorDef = new FixtureDef();
+    sensorDef.isSensor = true;
+    CircleShape sensorShape = new CircleShape();
+    // sensor has 0 radius, maybe modify?
+    sensorShape.setRadius(0);
+    sensorShape.setPosition(sensorCenter);
+    sensorDef.shape = sensorShape;
 
-        Fixture sensorFixture = bodies.get(0).getBody().createFixture( sensorDef );
-        sensorFixture.setUserData(getPointSensorName());
+    Fixture sensorFixture = bodies.get(0).getBody().createFixture(sensorDef);
+    sensorFixture.setUserData(getPointSensorName());
 
-        return true;
-    }
+    return true;
+  }
 }
