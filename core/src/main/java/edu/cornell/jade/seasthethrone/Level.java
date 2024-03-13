ba55c2ef07684010b32dc99f79e3bc3a3fa38bc3
David Kim <davidskim02@gmail.com>
2024-03-12 22:33:27 -0400

    Implement getLayer

--- core/src/main/java/edu/cornell/jade/seasthethrone/Level.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/Level.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.utils.Array;
 import edu.cornell.jade.seasthethrone.gamemodel.*;
 import edu.cornell.jade.seasthethrone.util.JsonHandler;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -28,19 +27,31 @@ public class Level {
 
     private HashMap<String, Object> background;
 
+    private Array<HashMap<String, Object>> layers;
+
 
     public Level(String fileName) {
-        player = new PlayerModel(0,0);
-        bosses = new Array<>();
-        enemies = new Array<>();
-
         HashMap<String, Object> levelMap = JsonHandler.jsonToMap(fileName);
+        layers = (Array<HashMap<String, Object>>)levelMap.get("layers");
 
-        Array<HashMap<String, Object>> layers = (Array<HashMap<String, Object>>)levelMap.get("layers");
+    }
 
-        background = layers.get(0);
-
-
+    /**
+     * Returns the layer with the given name
+     *
+     * @param layerName the name of the layer to return
+     *
+     * @return the layer with the given name
+     *
+     * @throws Error if the provided name doesn't match any layer in the level
+     * */
+    private HashMap<String, Object> getLayer(String layerName) {
+        for (HashMap<String, Object> layer : layers) {
+            if ((String) layer.get("name") == layerName) {
+                return layer;
+            }
+        }
+        throw new Error("No layer with name " + layerName);
     }
 
 }
