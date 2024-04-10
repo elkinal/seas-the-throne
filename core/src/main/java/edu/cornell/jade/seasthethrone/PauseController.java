b00534447387ace9422f6fd9da4767279668370c
phazrael <angelzhua@gmail.com>
2024-04-09 21:03:03 -0400

    slightly buggy pausecontroller

---
+++ core/src/main/java/edu/cornell/jade/seasthethrone/PauseController.java
@@ -0,0 +1,11 @@
+package edu.cornell.jade.seasthethrone;
+
+import edu.cornell.jade.seasthethrone.physics.PhysicsEngine;
+import edu.cornell.jade.seasthethrone.render.RenderingEngine;
+
+public class RunController {
+  // fields the player, the rendering engine
+  PlayerController player;
+  RenderingEngine render;
+  PhysicsEngine physics;
+}
