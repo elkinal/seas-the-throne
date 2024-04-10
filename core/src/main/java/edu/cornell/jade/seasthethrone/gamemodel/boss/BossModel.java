43492eb196e631e151c94452a4617ee01479aeb5
tl558 <tl558@cornell.edu>
2024-04-10 16:22:39 -0400

    fix for jelly animation

--- core/src/main/java/edu/cornell/jade/seasthethrone/gamemodel/boss/BossModel.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/gamemodel/boss/BossModel.java
@@ -276,8 +276,7 @@ public abstract class BossModel extends EnemyModel implements Renderable {
 
     public Builder setFalloverAnimation(Texture texture) {
       int width = texture.getWidth();
-      // TODO: Stop hardcoding columns
-      dieAnimation = new FilmStrip(texture, 1, 16);
+      dieAnimation = new FilmStrip(texture, 1, width / frameSize);
       ;
       return this;
     }
