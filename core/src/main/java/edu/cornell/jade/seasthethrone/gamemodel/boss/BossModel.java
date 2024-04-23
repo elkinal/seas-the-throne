0f6ff854a8a03bbf34ce3e82dc15f72550f155f4
Eugene Choi <4eugenechoi@gmail.com>
2024-04-22 23:55:26 -0400

    Crab hitbox

--- core/src/main/java/edu/cornell/jade/seasthethrone/gamemodel/boss/BossModel.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/gamemodel/boss/BossModel.java
@@ -338,16 +338,35 @@ public abstract class BossModel extends EnemyModel implements Renderable {
       float[] hitbox;
       switch (type) {
         case "crab":
-          hitbox = new float[]{-4, -7, -4, 7, 4, 7, 4, -7};
+          hitbox = new float[]{
+            -6.5f,  7,
+            -5,     8.5f,
+            -1.5f,  4,
+            -0.35f, 4.6f,
+            0.7f,   4,
+            1,      5.5f,
+            4,      7,
+            5.5f,   7,
+            5.5f,   5,
+            1.3f,   3,
+            6,      4,
+            5.5f,   -4,
+            3,      1.8f,
+            2,      -8,
+            -3,     -7,
+            -4,     -2,
+            -3,     0,
+            -6,     4
+          };
           break;
         case "jelly":
           hitbox = new float[]{
-                  -2,    1.2f,
-                  -0.6f, 2.5f,
-                  1,     2.5f,
-                  2.4f,  1.2f,
-                  2.4f,  -2.5f,
-                  -2,    -2.5f
+            -2,     1.2f,
+            -0.6f,  2.5f,
+            1,      2.5f,
+            2.4f,   1.2f,
+            2.4f,   -2.5f,
+            -2,     -2.5f
           };
           break;
         default:
