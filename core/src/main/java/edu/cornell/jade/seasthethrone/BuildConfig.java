bcfa5b8a305de02293ebe792b959fd83b00a2dce
David Kim <davidskim02@gmail.com>
2024-05-16 14:03:49 -0400

    checkpoint field for hub portals

--- core/src/main/java/edu/cornell/jade/seasthethrone/BuildConfig.java
+++ core/src/main/java/edu/cornell/jade/seasthethrone/BuildConfig.java
@@ -3,5 +3,5 @@ package edu.cornell.jade.seasthethrone;
 /** Variables specifying settings about the build. */
 public class BuildConfig {
   /** If the code should be build with debug information. */
-  public static final boolean DEBUG = false;
+  public static final boolean DEBUG = true;
 }
